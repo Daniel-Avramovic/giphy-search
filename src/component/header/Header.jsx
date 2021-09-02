f6f8875273195d3e515813f1d37bd4c7c3f5fd25
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-02 15:09:03 +0200

    create header component

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className="container">
+            Giphy App
+        </header>
+    )
+}
+
+export default Header;
