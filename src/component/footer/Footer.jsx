70cab4d4f17615afec96d5a821586f68a5deebb4
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-04 16:22:15 +0200

    add footer

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return(
+        <footer class="container-fluid bg-primary text-white text-center p-2">Copyrigth © BIT Student Daniel Avramovic 2021</footer>
+    )
+}
+
+export default Footer;
